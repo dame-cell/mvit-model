c276680afdc3a29aeb30156b262e800fd29b96ad
DAME RAJEE <doss72180@gmail.com>
2024-09-14 11:35:06 +0000

    added inferefcnce code modfify

--- mvit/models/inference.py
+++ mvit/models/inference.py
@@ -1,5 +1,8 @@
 from mvit_model import MViT 
 from mvit.config.defaults import get_cfg 
+from PIL import Image 
+import requests 
+import matplotlib.pyplot as plt 
 
 def count_parameters(model):
     """
@@ -14,6 +17,15 @@ def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
 
+url = "https://assets-c4akfrf5b4d3f4b7.z01.azurefd.net/assets/2024/04/BMDataViz_661fb89f3845e.png" 
+image = Image.open(requests.get(url, stream=True).raw) 
+
+
 config = get_cfg()
 model = MViT(config)
-print(count_parameters(model))
+pretrained_weights = torch.load("MViTv2_T_in1k.pyth")
+
+model.load_state_dict(pretrained_weights['model']) 
+plt.imshow(image)
+plt.show()
+print(f"Total trainable parameters: {count_parameters(model)}")
